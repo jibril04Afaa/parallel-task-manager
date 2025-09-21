6cbacd51c9618d23a1b3c9ae48480b87bbd43d52
Jibril <128831088+jibril04Afaa@users.noreply.github.com>
2025-09-21 02:30:59 -0400

    horsing around with processes

---
+++ demo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("We are in the demo program!\n");
+}
--- main.c
+++ main.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char **argv)
 {
-    printf("Hello from Parallel Task Manager! ");
+    // duplicate process
+    pid_t pid = fork();
+    char *args[] = {"Hello", "from", "main.c!", NULL};
+
+    if (pid == 0)
+    {
+        execv("./demo", args);
+        perror("Exec failed!\n");
+        exit(1); 
+    }
+    else if (pid > 0)
+    {
+        printf("This is a parent process with PID of %d\n", getpid());
+        wait(NULL); // wait for the child to finish
+    }
+    else
+    {
+        perror("Error forking\n");
+    }
+
     return 0;
 }
